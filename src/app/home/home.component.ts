7852170330d43cab844ffb2f512dc160e1ca93a2
Amir YAZIDI <amir.yazidi1@esprit.tn>
2024-02-06 14:07:08 +0100

    dataBinding

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  color:string="red"
+  title : string = "welcome 4BI3"
+  listProduct:any=[
+    {id:1,title:'t-shirt1',price:20,quantity:3,like:0},
+    {id:2,title:'t-shirt2',price:5,quantity:500,like:2},
+    {id:3,title:'t-shirt3',price:10,quantity:0,like:5},
+  ]
+
+  msg(){
+    alert("hello")
+  }
+  increment(id:number){
+    this.listProduct[id].like++
+  }
+  buy(id:number){
+    this.listProduct[id].quantity--
+
+  }
+}
